2368f770586c44423a6833782148c0030608404c
Ferenc Bondar <fhery021@gmail.com>
2020-02-28 16:22:20 +0000

    it compiles

--- app/src/main/java/com/fhery021/displayshelter/ui/home/HomeFragment.java
+++ app/src/main/java/com/fhery021/displayshelter/ui/home/HomeFragment.java
@@ -1,9 +1,16 @@
 package com.fhery021.displayshelter.ui.home;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -14,9 +21,31 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.fhery021.displayshelter.R;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Set;
+import java.util.UUID;
+
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+
+    BluetoothAdapter bluetoothAdapter;
+    BluetoothSocket bluetoothSocket;
+    BluetoothDevice bluetoothDevice;
+
+    OutputStream outputStream;
+    InputStream inputStream;
+    Thread thread;
+
+    byte[] readBuffer;
+    int readBufferPosition;
+    volatile boolean stopWorker;
+
+    TextView lblPrinterName;
+    EditText textBox;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -25,5 +54,171 @@ public class HomeFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
 
         return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Object of controls
+        Button btnConnect = (Button) view.findViewById(R.id.btnConnect);
+        Button btnDisconnect = (Button) view.findViewById(R.id.btnDisconnect);
+        Button btnPrint = (Button) view.findViewById(R.id.btnPrint);
+
+        textBox = (EditText) view.findViewById(R.id.txtText);
+
+        lblPrinterName = (TextView) view.findViewById(R.id.lblPrinterName);
+
+        btnConnect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    findBluetoothDevice();
+                    openBluetoothPrinter();
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+        btnDisconnect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    disconnectBT();
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+        btnPrint.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    printData();
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+    }
+
+    void findBluetoothDevice() {
+        try {
+            bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+            if (bluetoothAdapter == null) {
+                lblPrinterName.setText("No bluetooth device found");
+            }
+            if (bluetoothAdapter.isEnabled()) {
+                Intent enableBT = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBT, 0);
+            }
+
+            Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+
+            if (pairedDevices.size() > 0) {
+                for (BluetoothDevice pairedDevice : pairedDevices) {
+                    if (pairedDevice.getName().equals("MY_DEVICE_NAME")) {
+                        bluetoothDevice = pairedDevice;
+                        lblPrinterName.setText("Attached device name: " + pairedDevice.getName()); // TODO
+                        break;
+                    }
+                }
+            }
+            lblPrinterName.setText("Bluetooth device attached");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    void openBluetoothPrinter() throws IOException {
+        try {
+            UUID uuidString = UUID.fromString("0100"); // TODO
+            bluetoothSocket = bluetoothDevice.createRfcommSocketToServiceRecord(uuidString);
+            bluetoothSocket.connect();
+            outputStream = bluetoothSocket.getOutputStream();
+            inputStream = bluetoothSocket.getInputStream();
+
+            beginListenData();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    void beginListenData() {
+        try {
+            final Handler handler = new Handler();
+            final byte delimiter = 10;
+            stopWorker = false;
+            readBufferPosition = 0;
+            readBuffer = new byte[1024];
+
+            thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while (!Thread.currentThread().isInterrupted() && !stopWorker) {
+                        try {
+                            int byteAvailable = inputStream.available();
+                            if (byteAvailable > 0) {
+                                byte[] packetByte = new byte[byteAvailable];
+                                inputStream.read(packetByte);
+
+                                for (int i = 0; i < byteAvailable; i++) {
+                                    byte b = packetByte[i];
+                                    if (b == delimiter) {
+                                        byte[] encodedByte = new byte[readBufferPosition];
+                                        System.arraycopy(
+                                                readBuffer, 0,
+                                                encodedByte, 0,
+                                                encodedByte.length
+                                        );
+                                        final String data = new String(encodedByte, "US-ASCII");
+                                        readBufferPosition = 0;
+                                        handler.post(new Runnable() {
+                                            @Override
+                                            public void run() {
+                                                lblPrinterName.setText(data);
+                                            }
+                                        });
+                                    } else {
+                                        readBuffer[readBufferPosition++] = b;
+                                    }
+                                }
+                            }
+                        } catch (Exception ex) {
+                            stopWorker = true;
+                            ex.printStackTrace();
+                        }
+                    }
+                }
+            });
+            thread.start();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    // printing text to bt printer
+    void printData() throws IOException {
+        try {
+            String msg = textBox.getText().toString();
+            msg += "\n";
+            outputStream.write(msg.getBytes());
+            lblPrinterName.setText("Printing text ...");
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    void disconnectBT() throws IOException {
+        try {
+            stopWorker = true;
+            outputStream.close();
+            inputStream.close();
+            bluetoothSocket.close();
+            lblPrinterName.setText("Device disconnected");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
