849a74d694531138f7964b7a7abb71b3d3c50191
Ferenc Bondar <fhery021@gmail.com>
2020-02-28 15:43:00 +0000

    first version and untested

--- app/src/main/java/com/fhery021/displayshelter/MainActivity.java
+++ app/src/main/java/com/fhery021/displayshelter/MainActivity.java
@@ -1,6 +1,15 @@
 package com.fhery021.displayshelter;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -10,7 +19,28 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Set;
+import java.util.UUID;
+
 public class MainActivity extends AppCompatActivity {
+
+    BluetoothAdapter bluetoothAdapter;
+    BluetoothSocket bluetoothSocket;
+    BluetoothDevice bluetoothDevice;
+
+    OutputStream outputStream;
+    InputStream inputStream;
+    Thread thread;
+
+    byte[] readBuffer;
+    int readBufferPosition;
+    volatile boolean stopWorker;
+
+    TextView lblPrinterName;
+    EditText textBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +55,167 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
+        // Object of controls
+        Button btnConnect = (Button) findViewById(R.id.btnConnect);
+        Button btnDisconnect = (Button) findViewById(R.id.btnDisconnect);
+        Button btnPrint = (Button) findViewById(R.id.btnPrint);
+
+        textBox = (EditText) findViewById(R.id.txtText);
+
+        lblPrinterName = (TextView) findViewById(R.id.lblPrinterName);
+
+        btnConnect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    findBluetoothDevice();
+                    openBluetoothPrinter();
+                }catch (Exception ex){
+                    ex.printStackTrace();
+                }
+            }
+        });
+        btnDisconnect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    disconnectBT();
+                }catch (Exception ex){
+                    ex.printStackTrace();
+                }
+            }
+        });
+        btnPrint.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    printData();
+                }catch (Exception ex){
+                    ex.printStackTrace();
+                }
+            }
+        });
+    }
+
+    void findBluetoothDevice() {
+        try {
+            bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+            if (bluetoothAdapter == null) {
+                lblPrinterName.setText("No bluetooth device found");
+            }
+            if (bluetoothAdapter.isEnabled()) {
+                Intent enableBT = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBT, 0);
+            }
+
+            Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+
+            if (pairedDevices.size() > 0) {
+                for (BluetoothDevice pairedDevice : pairedDevices) {
+                    if (pairedDevice.getName().equals("MY_DEVICE_NAME")) {
+                        bluetoothDevice = pairedDevice;
+                        lblPrinterName.setText("Attached device name: " + pairedDevice.getName()); // TODO
+                        break;
+                    }
+                }
+            }
+            lblPrinterName.setText("Bluetooth device attached");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    void openBluetoothPrinter() throws IOException {
+        try {
+            UUID uuidString = UUID.fromString("0100"); // TODO
+            bluetoothSocket = bluetoothDevice.createRfcommSocketToServiceRecord(uuidString);
+            bluetoothSocket.connect();
+            outputStream = bluetoothSocket.getOutputStream();
+            inputStream = bluetoothSocket.getInputStream();
+
+            beginListenData();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    void beginListenData() {
+        try {
+            final Handler handler = new Handler();
+            final byte delimiter = 10;
+            stopWorker = false;
+            readBufferPosition = 0;
+            readBuffer = new byte[1024];
+
+            thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while (!Thread.currentThread().isInterrupted() && !stopWorker) {
+                        try {
+                            int byteAvailable = inputStream.available();
+                            if (byteAvailable > 0) {
+                                byte[] packetByte = new byte[byteAvailable];
+                                inputStream.read(packetByte);
+
+                                for (int i = 0; i < byteAvailable; i++) {
+                                    byte b = packetByte[i];
+                                    if (b == delimiter) {
+                                        byte[] encodedByte = new byte[readBufferPosition];
+                                        System.arraycopy(
+                                                readBuffer, 0,
+                                                encodedByte, 0,
+                                                encodedByte.length
+                                        );
+                                        final String data = new String(encodedByte, "US-ASCII");
+                                        readBufferPosition = 0;
+                                        handler.post(new Runnable() {
+                                            @Override
+                                            public void run() {
+                                                lblPrinterName.setText(data);
+                                            }
+                                        });
+                                    } else {
+                                        readBuffer[readBufferPosition++] = b;
+                                    }
+                                }
+                            }
+                        } catch (Exception ex) {
+                            stopWorker = true;
+                            ex.printStackTrace();
+                        }
+                    }
+                }
+            });
+            thread.start();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    // printing text to bt printer
+    void printData() throws IOException {
+        try {
+            String msg = textBox.getText().toString();
+            msg += "\n";
+            outputStream.write(msg.getBytes());
+            lblPrinterName.setText("Printing text ...");
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    void disconnectBT() throws IOException {
+        try {
+            stopWorker = true;
+            outputStream.close();
+            inputStream.close();
+            bluetoothSocket.close();
+            lblPrinterName.setText("Device disconnected");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 
 }
---
+++ app/src/main/java/com/fhery021/displayshelter/bluetooth/BluetoothPrinter.java
@@ -0,0 +1,18 @@
+package com.fhery021.displayshelter.bluetooth;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import java.util.Set;
+import java.util.UUID;
+
+public class BluetoothPrinter {
+
+   // extract from main activity maybe ?
+}
