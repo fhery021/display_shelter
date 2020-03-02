7c369bd92d73565cd0de0761490bef023af830db
Ferenc Bondar <fhery021@gmail.com>
2020-03-02 16:15:27 +0200

    nothing is working

--- app/src/main/java/com/fhery021/displayshelter/bluetooth/MyPrintDocumentAdapter.java
+++ app/src/main/java/com/fhery021/displayshelter/bluetooth/MyPrintDocumentAdapter.java
@@ -1,10 +1,25 @@
 package com.fhery021.displayshelter.bluetooth;
 
+import android.os.Bundle;
+import android.os.CancellationSignal;
+import android.os.ParcelFileDescriptor;
+import android.print.PageRange;
+import android.print.PrintAttributes;
 import android.print.PrintDocumentAdapter;
 
 import androidx.fragment.app.FragmentActivity;
 
 public class MyPrintDocumentAdapter extends PrintDocumentAdapter {
     public MyPrintDocumentAdapter(FragmentActivity activity) {
+    }
+
+    @Override
+    public void onLayout(PrintAttributes oldAttributes, PrintAttributes newAttributes, CancellationSignal cancellationSignal, LayoutResultCallback callback, Bundle extras) {
+
+    }
+
+    @Override
+    public void onWrite(PageRange[] pages, ParcelFileDescriptor destination, CancellationSignal cancellationSignal, WriteResultCallback callback) {
+
     }
 }
--- app/src/main/java/com/fhery021/displayshelter/ui/home/HomeFragment.java
+++ app/src/main/java/com/fhery021/displayshelter/ui/home/HomeFragment.java
@@ -1,47 +1,29 @@
 package com.fhery021.displayshelter.ui.home;
 
 import android.content.Context;
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.print.PrintManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.fhery021.displayshelter.R;
-import com.mazenrashed.printooth.Printooth;
-import com.mazenrashed.printooth.data.printable.ImagePrintable;
-import com.mazenrashed.printooth.data.printable.Printable;
-import com.mazenrashed.printooth.data.printable.RawPrintable;
-import com.mazenrashed.printooth.data.printable.TextPrintable;
-import com.mazenrashed.printooth.data.printer.DefaultPrinter;
-import com.mazenrashed.printooth.ui.ScanningActivity;
-import com.mazenrashed.printooth.utilities.Printing;
-import com.mazenrashed.printooth.utilities.PrintingCallback;
-import com.squareup.picasso.Picasso;
-import com.squareup.picasso.Target;
+import com.fhery021.displayshelter.bluetooth.MyPrintDocumentAdapter;
 
-import java.util.ArrayList;
+public class HomeFragment extends Fragment  {
 
-public class HomeFragment extends Fragment implements PrintingCallback {
 
-    // direct net path
-    // private static final String IMAGE_PATH = "https://i.ya-webdesign.com/images/small-circle-png-9.png";
-    // firebase png
-    private static final String IMAGE_PATH = "https://firebasestorage.googleapis.com/v0/b/mysvgprovider.appspot.com/o/small-circle-png-9%5B1%5D.png?alt=media&token=efc981c6-4f1b-47f3-a7b8-8a256cc50db7";
-    // firebase svg
-//    private static final String IMAGE_PATH = "https://firebasestorage.googleapis.com/v0/b/mysvgprovider.appspot.com/o/small-circle-png-9_1_.svg?alt=media&token=ea7ec6a1-e529-4050-8719-e1d3ee4a56ee";
-
-    Printing printing;
-    Button btn_pair_unpair, btn_print, btn_print_image;
+    TextView lbl_status;
+    Button btn_pair, btn_print;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -54,195 +36,45 @@ public class HomeFragment extends Fragment implements PrintingCallback {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
-        Picasso.get().setLoggingEnabled(true);
-
-        if (Printooth.INSTANCE.hasPairedPrinter())
-            printing = Printooth.INSTANCE.printer();
-
         initView(view);
 
     }
 
-    private void initView(View view) {
+    private void initView (View view) {
         btn_print = view.findViewById(R.id.btnPrint);
-        btn_pair_unpair = view.findViewById(R.id.btnPairUnpair);
-        btn_print_image = view.findViewById(R.id.btnPrintImage);
-
-        if (printing != null) {
-            printing.setPrintingCallback(this);
-        }
-        //Event
-        btn_pair_unpair.setOnClickListener(v -> {
-            if (Printooth.INSTANCE.hasPairedPrinter()) {
-                Printooth.INSTANCE.removeCurrentPrinter();
-            } else {
-                startActivityForResult(new Intent(getActivity(), ScanningActivity.class), ScanningActivity.SCANNING_FOR_PRINTER);
-                changePairAndUnpair();
-            }
-        });
-
-        btn_print_image.setOnClickListener(v -> {
-            if (!Printooth.INSTANCE.hasPairedPrinter()) {
-                startActivityForResult(new Intent(getActivity(), ScanningActivity.class), ScanningActivity.SCANNING_FOR_PRINTER);
-            } else {
-                printImages();
-            }
-        });
+        btn_pair = view.findViewById(R.id.btnPair);
 
         btn_print.setOnClickListener(v -> {
-            if (!Printooth.INSTANCE.hasPairedPrinter()) {
-                startActivityForResult(new Intent(getActivity(), ScanningActivity.class), ScanningActivity.SCANNING_FOR_PRINTER);
-            } else {
-                printText();
-            }
+            doPrint();
         });
 
-        changePairAndUnpair();
+        btn_pair.setOnClickListener(v -> {
+            showToast("Pair selected...");
+        });
     }
+    private void doPrint() {
+        showToast("Begin printing process");
+        // Get a PrintManager instance
+        PrintManager printManager = (PrintManager) getActivity()
+                .getSystemService(Context.PRINT_SERVICE);
 
-    private void printText() {
-        ArrayList<Printable> printables = new ArrayList<>();
-        printables.add(
-                new RawPrintable.Builder
-                        (new byte[]{27, 100, 4})
-                        .build());
+        // Set job name, which will be displayed in the print queue
+        String jobName = getActivity().getString(R.string.app_name) + " Document";
 
-        // add text
-        printables.add(
-                new TextPrintable.Builder()
-                        .setText("Hello")
-                        .setCharacterCode(DefaultPrinter.Companion.getCHARCODE_PC1252())
-                        .setNewLinesAfter(1)
-                        .build());
-
-        // Custom text
-        printables.add(
-                new TextPrintable.Builder()
-                        .setText("Hali")
-                        .setLineSpacing(DefaultPrinter.Companion.getLINE_SPACING_60())
-                        .setAlignment(DefaultPrinter.Companion.getALIGNMENT_CENTER())
-                        .setEmphasizedMode(DefaultPrinter.Companion.getEMPHASIZED_MODE_BOLD())
-                        .setUnderlined(DefaultPrinter.Companion.getUNDERLINED_MODE_ON())
-                        .setNewLinesAfter(1)
-                        .build()
-        );
-        // TODO TRY PRINTING TEXT
-        // printing.print(printables);
-        Printooth.INSTANCE.printer().print(printables);
-    }
-
-    private void printImages() {
-        ArrayList<Printable> printables = new ArrayList<>();
-
-        // load image from the Internet
-        Picasso.get()
-                .load(IMAGE_PATH)
-                .into(new Target() {
-                    @Override
-                    public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
-                        printables.add(new ImagePrintable.Builder(bitmap).build());
-////                        printing.print(printables);
-//                        // TODO TRY THIS:
-//                        Printooth.INSTANCE.printer().print(printables);
-
-                        // TODO TRY THIS:
-                        // version 2
-//                        PrintHelper photoPrinter = new PrintHelper(getActivity());
-//                        photoPrinter.setScaleMode(PrintHelper.SCALE_MODE_FIT);
-//                        photoPrinter.printBitmap("circle - test print", bitmap);
-
-//                        outputStream.write(PrinterCommands.ESC_ALIGN_CENTER);
-//                        printText(command);
-                        Printooth.INSTANCE.printer().print(printables);
-
-                    }
-
-                    @Override
-                    public void onBitmapFailed(Exception e, Drawable errorDrawable) {
-                        showToast("Failed to load image");
-                        e.printStackTrace();
-                    }
-
-                    @Override
-                    public void onPrepareLoad(Drawable placeHolderDrawable) {
-                        showToast("Loading image");
-                    }
-                });
-    }
-
-    //https://developer.android.com/training/printing/photos
-
-//    private void doPhotoPrint() {
-//        PrintHelper photoPrinter = new PrintHelper(getActivity());
-//        photoPrinter.setScaleMode(PrintHelper.SCALE_MODE_FIT);
-//        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),
-//                R.drawable.droids);
-//        photoPrinter.printBitmap("droids.jpg - test print", bitmap);
-//    }
-
-    private void changePairAndUnpair() {
-        if (Printooth.INSTANCE.hasPairedPrinter()) {
-            btn_pair_unpair.setText(
-                    new StringBuilder("Unpair ")
-                            .append(Printooth.INSTANCE.getPairedPrinter()
-                                    .getName()
-                                    .toString())
-            );
-        } else {
-            btn_pair_unpair.setText("Pair with Printer");
-        }
-    }
-
-    @Override
-    public void connectingWithPrinter() {
-        showToast("Connecting...");
-    }
-
-    @Override
-    public void connectionFailed(String s) {
-        showToast("Failure: " + s);
-    }
-
-    @Override
-    public void onError(String s) {
-        showToast("Error: " + s);
-    }
-
-    @Override
-    public void onMessage(String s) {
-        showToast(s);
-    }
-
-    @Override
-    public void printingOrderSentSuccessfully() {
-        showToast("Order sent to device.");
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == ScanningActivity.SCANNING_FOR_PRINTER &&
-                resultCode == ScanningActivity.RESULT_OK) {
-            initPrinting();
-            changePairAndUnpair();
-        }
-    }
-
-    private void initPrinting() {
-        if (!Printooth.INSTANCE.hasPairedPrinter()) {
-            printing = Printooth.INSTANCE.printer();
-        }
-
-        if (printing != null) {
-            printing.setPrintingCallback(this);
-        }
+        // Start a print job, passing in a PrintDocumentAdapter implementation
+        // to handle the generation of a print document
+        printManager.print(jobName, new MyPrintDocumentAdapter(getActivity()),
+                null); //
     }
 
     private void showToast(String msg) {
         Context context = getActivity().getApplicationContext();
         CharSequence text = msg;
 
-        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
+        Toast.makeText(context, msg, Toast.LENGTH_LONG).show();
+        Log.println(Log.INFO,"MyTOAST", msg);
     }
+
+
+
 }
