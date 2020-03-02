73cde2f49860d9c42781f9a514ec0fa699f5b231
Ferenc Bondar <fhery021@gmail.com>
2020-03-02 16:15:19 +0200

    nothing is working

---
+++ app/src/main/java/com/fhery021/displayshelter/bluetooth/MyPrintDocumentAdapter.java
@@ -0,0 +1,10 @@
+package com.fhery021.displayshelter.bluetooth;
+
+import android.print.PrintDocumentAdapter;
+
+import androidx.fragment.app.FragmentActivity;
+
+public class MyPrintDocumentAdapter extends PrintDocumentAdapter {
+    public MyPrintDocumentAdapter(FragmentActivity activity) {
+    }
+}
